6e1680a13bc945ba22174ea598b2ae131c323eeb
litb47 <75122870+litb47@users.noreply.github.com>
2025-12-05 13:57:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',              // <<< השורה החדשה הקריטית
   plugins: [react()],
   root: './',
   publicDir: 'public',
@@ -18,8 +19,8 @@ export default defineConfig({
             return 'assets/[name]-[hash][extname]';
           }
           return 'assets/[name]-[hash][extname]';
-        }
-      }
-    }
+        },
+      },
+    },
   },
 });
