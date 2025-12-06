afd57f2f27f5363f73252bcd6a922c9f4d1f93ab
litb47 <75122870+litb47@users.noreply.github.com>
2025-12-06 19:05:04 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,7 +30,7 @@ export const Hero: React.FC = () => {
               </span>
               
               {/* מילה שניה: Frank */}
-              <span className="font-frank text-5xl md:text-6xl lg:text-7xl block -mt-4">
+              <span className="font-frank text-5xl md:text-6xl lg:text-7xl block -mt-3">
                 לגיבורים
               </span>
             </h2>
